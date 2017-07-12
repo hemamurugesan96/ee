c40257b88845fe4d7a35a46d9b7d8b3aa7a5fb63
Hema M <hemamurugesanece@gmail.com>
2017-07-12 11:12:10 +0530

    Conversion.java

---
+++ Conversion.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Scanner;
+class Conversion
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+String str=sc.nextLine();
+int n=Integer.parseInt(str);
+System.out.println(n);
+}
+}
